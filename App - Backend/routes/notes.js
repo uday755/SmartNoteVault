b7262b1c205f07f0ab3793b17a960746d3418c2d
uday755 <79998578+uday755@users.noreply.github.com>
2024-01-30 02:06:12 +0530

    Implemented Update Note API

--- App - Backend/routes/notes.js
+++ App - Backend/routes/notes.js
@@ -39,4 +39,25 @@ noteRouter.post("/addNote", fetchUser, [
         }
     })
 
+// ROUTE :3 :: Update an existing Note for User using : PUT "/api/notes/updateNote" . Login Required
+noteRouter.put("/updateNote/:id", fetchUser, async (req, res)=>{
+    const {title, description, tag} = req.body;
+    // Create a newNote object
+    const newNote = {};
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+
+    // Find the note to be updated and update it
+    let note = await NoteModel.findById(req.params.id);
+    if(!note){return res.status(404).send("Not Found")}
+
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Unauthorised Access");
+    }
+
+    note = await NoteModel.findByIdAndUpdate(req.params.id, {$set : newNote}, {new : true});
+    res.json({note});
+})
+
 module.exports = noteRouter;
