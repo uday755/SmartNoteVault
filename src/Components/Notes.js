057008630e6dc172df0736320a7040594dd9ed52
uday755 <79998578+uday755@users.noreply.github.com>
2024-02-03 08:08:26 +0530

    Updated Notes on Frontend and backend working fine

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -5,29 +5,29 @@ import AddNote from './AddNote';
 
 const Notes = () => {
     const context = useContext(NoteContext);
-    const { notes, getNotes } = context;
+    const { notes, getNotes, editNote } = context;
     useEffect(() => {
         getNotes();
         // eslint-disable-next-line
     }, []);
 
     const ref = useRef(null);
-    const [note, setNote] = useState({ etitle: "", edescription: "", etag: "" })
+    const refClose = useRef(null);
+    const [note, setNote] = useState({ id: "", etitle: "", edescription: "", etag: "" })
 
     const updateNote = (currentNote) => {
         ref.current.click();
-        setNote({etitle:currentNote.title, edescription:currentNote.description, etag: currentNote.tag});
+        setNote({ id: currentNote._id, etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag });
 
     }
 
     const handleClick = (e) => {
-        console.log("Updating the Note ...", note)
-        e.preventDefault();
+        console.log("Updating the Note ...", note);
+        editNote(note.id, note.etitle, note.edescription, note.etag)
+        refClose.current.click();
     }
     const onChange = (e) => {
         setNote({ ...note, [e.target.name]: e.target.value })
-
-
     }
 
 
@@ -63,7 +63,7 @@ const Notes = () => {
                             </form>
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" ref={refClose}>Close</button>
                             <button type="button" className="btn btn-primary" onClick={handleClick}>Update Note</button>
                         </div>
                     </div>
@@ -74,7 +74,7 @@ const Notes = () => {
             <div className="row mx-3 my-2">
                 <h3>Your Notes</h3>
                 {notes.map((note) => {
-                    return <NoteItem key={note._id}  updateNote={updateNote} note={note} />
+                    return <NoteItem key={note._id} updateNote={updateNote} note={note} />
                 })}
             </div>
         </>
--- src/Context/notes/NoteState.js
+++ src/Context/notes/NoteState.js
@@ -31,7 +31,8 @@ const NoteState = (props) => {
       },
       body: JSON.stringify({ title, description, tag }),
     });
-    console.log(response);
+    const json = await response.json();
+    console.log(json);
 
     const note =
     {
@@ -68,23 +69,30 @@ const NoteState = (props) => {
   const editNote = async (id, title, description, tag) => {
     // API Call 
     const response = await fetch(`${host}/api/notes/updateNote/${id}`, {
-      method: "POST",
+      method: "PUT",
       headers: {
         "Content-Type": "application/json",
         "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjViNmJhZTVmNWIzODQ2ZDg3MDNmMjFhIn0sImlhdCI6MTcwNjQ3NDI4MX0.8O3AtsClfK0Xlc1WA80bwXfu2minNNxA2SW00IgAvhQ"
       },
       body: JSON.stringify({ title, description, tag }),
     });
-    const json = response.json();
+    const json = await response.json();
+    console.log(json);
+
+    let newNotes = JSON.parse(JSON.stringify(notes))
     // Logic to edit in client
-    for (let index = 0; index < notes.length; index++) {
-      const element = notes[index];
+    for (let index = 0; index < newNotes.length; index++) {
+      const element = newNotes[index];
       if (element._id === id) {
-        element.title = title;
-        element.description = description;
-        element.tag = tag;
+        // newNotes[index]._id = id;
+        newNotes[index].title = title;
+        newNotes[index].description = description;
+        newNotes[index].tag = tag;
+        break;
       }
     }
+    console.log(newNotes);
+    setNotes(newNotes);
   }
 
   return (
