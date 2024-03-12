762a2c74ba088063b1cb2c03757c1e411f0dff41
uday755 <79998578+uday755@users.noreply.github.com>
2024-03-12 13:06:45 +0530

    Removed Unnecessary Alert

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import { Outlet } from 'react-router-dom'
 import NoteState from './Context/notes/NoteState';
-import Alert from './Components/Alert';
+// import Alert from './Components/Alert';
 
 function App() {
   return (
     <>
       <NoteState>
         <Navbar />
-        <Alert message ="This is amazing React Library" />
+        {/* <Alert message ="This is amazing React Library" /> */}
         <div className="container">
         <Outlet />
         </div>
